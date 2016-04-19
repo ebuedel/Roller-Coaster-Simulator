c0ed64c855d1562d818009a44c8d9132f78fe17c
Eric Buedel <ebuedel@purdue.edu>
2016-04-18 23:56:43 -0400

    did stuff

--- app.js
+++ app.js
@@ -7,6 +7,9 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var cradle = require('cradle');
+
+var db = new(cradle.Connection)().database('shizniz');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
