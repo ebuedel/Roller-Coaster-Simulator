0e5bf9c3935e224b9dc41df359ed624939e7263c
Eric Buedel <ebuedel@purdue.edu>
2016-04-19 23:03:03 -0400

    poo

--- app.js
+++ app.js
@@ -1,74 +1,248 @@
-var express = require('express');
 var cradle = require('cradle');
+//var cfenv = require('cfenv');
 
-var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
-var vcap = vcapServices.cloudantNoSQLDB[0];
+// Obtain the pouchdb interface from VCAP_SERVICES
+var http = require('http');
+if (process.env.VCAP_SERVICES) {
+      // Running on Bluemix. Parse the process.env for the port and host that we've been assigned.
+      var env = JSON.parse(process.env.VCAP_SERVICES);
+      var host = process.env.VCAP_APP_HOST; 
+      var port = process.env.VCAP_APP_PORT;
+      console.log('VCAP_SERVICES: %s', process.env.VCAP_SERVICES);    
+      // Also parse out Cloudant settings.
+      var cloudant = env['cloudantNoSQLDB'][0]['credentials'];
+}
 
-cradle.setup({
-    host: vcap.credentials.host,
-    port: vcap.credentials.port,
-    auth: {
-        username: vcap.credentials.username,
-        password: vcap.credentials.password 
+//Insert records into the books DB
+ var insert_records = function(req, res) {
+    //Parse the process.env for the port and host that we've been assigned
+    if (process.env.VCAP_SERVICES) {
+          // Running on Bluemix. Parse the port and host that we've been assigned.
+          var env = JSON.parse(process.env.VCAP_SERVICES);
+          var host = process.env.VCAP_APP_HOST; 
+          var port = process.env.VCAP_APP_PORT;
+
+          console.log('VCAP_SERVICES: %s', process.env.VCAP_SERVICES);    
+          // Also parse Cloudant settings.
+          var cloudant = env['cloudantNoSQLDB'][0]['credentials'];
     }
-});
+    
+    var db = new pouchdb('books'),
+     remote =cloudant.url + '/books';
+    opts = {
+      continuous: true
+      };
+     // Replicate the DB to remote
+    console.log(remote);
+    db.replicate.to(remote, opts);
+    db.replicate.from(remote, opts);
+    
+    // Put 3 documents into the DB
+    db.put({
+          author: 'John Grisham',
+          Title : 'The Firm'          
+        }, 'book1', function (err, response) {
+          console.log(err || response);
+        });
+     db.put({
+          author: 'Authur C Clarke',
+          Title : '2001: A Space Odyssey'         
+        }, 'book2', function (err, response) {
+          console.log(err || response);
+        });
+     db.put({
+          author: 'Dan Brown',
+          Title : 'Digital Fortress'          
+        }, 'book3', function (err, response) {
+          console.log(err || response);
+        });
+     res.writeHead(200, {'Content-Type': 'text/plain'});
+     res.write("3 documents is inserted");
+     res.end();
+}; // End insert_records
 
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// Update records in the books DB
+var update_records = function(req, res) {
+    if (process.env.VCAP_SERVICES) {
+          // Running on Bluemix. Parse for the port and host 
+          var env = JSON.parse(process.env.VCAP_SERVICES);
+          var host = process.env.VCAP_APP_HOST; 
+          var port = process.env.VCAP_APP_PORT;
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-    var connection = new cradle.Connection();
+          console.log('VCAP_SERVICES: %s', process.env.VCAP_SERVICES);    
 
-    console.log(JSON.stringify(connection));
-
-    var db = connection.database('testDataBase');
-    db.create(function(err){
-        console.log('some random error: ' + err);
+          // Also parse Cloudant settings.
+          var cloudant = env['cloudantNoSQLDB'][0]['credentials'];
+    }
+    
+    var db = new pouchdb('books'),
+    remote =cloudant.url + '/books';
+    opts = {
+      continuous: true
+      };
+     //Create a collection books
+    console.log(remote);
+    db.replicate.to(remote, opts);
+    db.replicate.from(remote, opts);
+    
+    // Update book3
+    db.get('book3', function(err, response) {
+        console.log(response);
+        return db.put({
+            _id: 'book3',
+            _rev: response._rev,
+            author: response.author,
+            Title : 'The da Vinci Code',              
+         });
+        }, function(err, response) {
+          if (err) {
+            console.log("error " + err);
+          } else {
+            console.log("Success " + response);
+          }
     });
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write("Updated the book3 document. Changed the Title from Digital Fortress to The Da Vinci Code");
+    res.end();
 
-});
+}; //End update-records
 
-//var connection = new(cradle.Connection)(host: vcap.credentials.host, port: vcap.credentials.port, auth: { username: vcap.credentials.username, password: vcap.credentials.password } );
-//console.log('connection?: ' connection);
+//Delete a document from the books DB
+var delete_record = function(req, res) {
+    
+    if (process.env.VCAP_SERVICES) {
+          // Running on Bluemix. Parse for the port and host that we've been assigned.
+          var env = JSON.parse(process.env.VCAP_SERVICES);
+          var host = process.env.VCAP_APP_HOST; 
+          var port = process.env.VCAP_APP_PORT;
+          console.log('VCAP_SERVICES: %s', process.env.VCAP_SERVICES);    
+          // Also parse out Cloudant settings.
+          var cloudant = env['cloudantNoSQLDB'][0]['credentials'];
+    }
+        
+    var db = new pouchdb('books'),
+      remote =cloudant.url + '/books';
+      opts = {
+        continuous: true
+      };
+     //Create a collection books
+    db.replicate.to(remote, opts);
+    db.replicate.from(remote, opts);
+    
+    //Deleting document book1
+    db.get('book1', function(err, doc) {
+        db.remove(doc, function(err, response) { 
+            console.log(err || response);
+        });
+    });
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write("Deleted book1");
+    res.end();
+}; //End delete-records
 
-//var connection = new(cradle.Connection)('887ef523-362d-4ad0-95d3-6fcfe60e4774-bluemix.cloudant.com', 443, { auth: { username: '887ef523-362d-4ad0-95d3-6fcfe60e4774-bluemix', password: '5f2846b2987b9ee2c26ed33bcac9c8129eb6647dd924f47ec7169ae2cf2b52d9'}
-//});
 
-//console.log(JSON.stringify(connection));
+//List Records from the books DB
+var list_records = function(req, res) {
+    
+    if (process.env.VCAP_SERVICES) {
+          // Running on Bluemix. Parse out the port and host that we've been assigned.
+          var env = JSON.parse(process.env.VCAP_SERVICES);
+          var host = process.env.VCAP_APP_HOST; 
+          var port = process.env.VCAP_APP_PORT;
+          console.log('VCAP_SERVICES: %s', process.env.VCAP_SERVICES);    
 
-//var db = connection.database('testing');
-//db.create(function(err){
-    //error
-//    console.log("error in db.create");
-//});
+          // Also parse out Cloudant settings.
+          var cloudant = env['cloudantNoSQLDB'][0]['credentials'];
+    }
+        
+    var db = new pouchdb('books'),
+      remote =cloudant.url + '/books';
+    opts = {
+      continuous: true
+     };
+        
+   //Create a collection books
+    console.log(remote);
+    db.replicate.to(remote, opts);
+    db.replicate.from(remote, opts);    
 
-/*db.save('someNewEntry', {
-    force: 'someValue',
-    name: 'someName'
-}, function (err, res) {
-    //nothing for now
-});*/
+    var docs = db.allDocs(function(err, response) { 
+        val = response.total_rows;      
+        var details = "";
+        j=0;
+        for(i=0; i < val; i++) {
+                        
+            
+            db.get(response.rows[i].id, function (err,doc){
+                 j++;   
+                details= details + JSON.stringify(doc.Title) + " by  " +  JSON.stringify(doc.author) + "\n";
+                // Kludge because of Node.js asynchronous handling. To be fixed - T V Ganesh
+                if(j == val) {
+                    
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    res.write(details);
+                    res.end();
+                    console.log(details);
+                }           
+               
+           }); // End db.get
+            
+        } //End for 
+    
+     }); // End db.allDocs
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+   
+  };
 
-// create a new express server
-var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+var port = (process.env.VCAP_APP_PORT || 1337);
+var host = (process.env.VCAP_APP_HOST || '0.0.0.0');
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+//Create a Webserver and wait for REST API CRUD calls
+require('http').createServer(function(req, res) {     
+    //Set up the DB connection
+     if (process.env.VCAP_SERVICES) {
+          // Running on Bluemix. Parse for  the port and host that we've been assigned.
+          var env = JSON.parse(process.env.VCAP_SERVICES);
+          var host = process.env.VCAP_APP_HOST; 
+          var port = process.env.VCAP_APP_PORT;
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+          console.log('VCAP_SERVICES: %s', process.env.VCAP_SERVICES);    
 
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+          // Also parse out Cloudant settings.
+          var cloudant = env['cloudantNoSQLDB'][0]['credentials'];
+     }
+    
+     var db = new pouchdb('books'),
+        remote =cloudant.url + '/books';      
+        opts = {
+          continuous: true
+        };     
+        console.log(remote);
+        db.replicate.to(remote, opts);
+        db.replicate.from(remote, opts);            
+        console.log("Reached3");
+        
+     // Perform CRUD operations through REST APIs
+        
+      // Insert document
+      if(req.method == 'POST') {
+                 insert_records(req,res);           
+      }
+      // List documents
+      else if(req.method == 'GET') {   
+              list_records(req,res);              
+       }
+       // Update a document
+       else if(req.method == 'PUT') {
+              update_records(req,res);
+        }
+        // Delete a document
+         else if(req.method == 'DELETE') {
+              delete_record(req,res);
+        }      
+  
+}).listen(port, host);
+console.log("Connected to port =" + port + " host =  " + host);
